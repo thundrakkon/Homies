d6faee0c9015dbb8d6ae0d090b5d349b1cdb37c0
thundrakkon <DuongPTNguyen@gmail.com>
2020-07-16 19:47:58 -0700

    JS files and CSV fixes

---
+++ logic.js
@@ -0,0 +1,12 @@
+var apiKey = API_KEY;
+
+const url = "https://api.spacexdata.com/v2/launchpads";
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+  console.log(data);
+});
+
+// Promise Pending
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
