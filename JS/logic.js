167682f8a56512b52b0da756db8a32c918cd48c5
thundrakkon <DuongPTNguyen@gmail.com>
2020-07-18 01:25:22 -0700

    JS updated with Json and Index fixes

--- JS/logic.js
+++ JS/logic.js
@@ -1,12 +1,25 @@
 var apiKey = API_KEY;
 
-const url = "http://127.0.0.1:5000/api/v1.0/data";
+// var url = "http://127.0.0.1:5000/api/v1.0/data";
+var url = "./Resources/city_join.json"
 
 // Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-  console.log(data);
-});
+d3.json(url).then((jsonData) => {
+  console.log(jsonData)
+
+  var city = jsonData.map(row => row.city)
+  var city_sorted = city.sort();
+  console.log(city_sorted)
+
+  d3.select("#selDataset")
+        .selectAll("option")
+        .data(city)
+        .enter().append("option")
+        .classed("cities", true)
+        .text(function(i) {return i;})
+        .exit().remove()
+})
 
 // Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+// const dataPromise = d3.json(url);
+// console.log("Data Promise: ", dataPromise);
